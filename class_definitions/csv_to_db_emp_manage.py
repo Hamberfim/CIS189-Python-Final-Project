424ec222f039218655b11a28213c57f4d39c8b42
Hamberfim <adhamlingithub@gmail.com>
2020-07-21 14:15:02 -0500

    initial functionality file docstring and steps todo

--- class_definitions/csv_to_db_emp_manage.py
+++ class_definitions/csv_to_db_emp_manage.py
@@ -4,20 +4,20 @@ Created on 7/21/2020
 @author: Anthony Hamlin
 Program: csv_to_db_emp_manage.py
 
-This program import a csv file and created a SQLite3 database
-and then is managed through a tkinter GUI.
+This program imports a csv file and creates a SQLite3 database
+and then the database is managed through a tkinter GUI.
+TODO:
+    Data Exchange:
+        * Read the CSV file,
+        * create a SQLite3 database with table,
+        * use first row headers for column names,
+        * populate the table with the data.
+    Build GUI on top with CRUD functionality
 """
-
-
-def stub_main():
-    """
-    Use reST style.
-    :param x: this is what the first parameter represents
-    :returns: this is what is returned
-    :raises keyError: raises an exception
-    """
-    pass
+import csv
+import sqlite3
+from sqlite3 import Error
 
 
 if __name__ == '__main__':
-    stub_main()
+    pass
