b20693b8530fa792f072c455b5524efa8af4a359
Hamberfim <adhamlingithub@gmail.com>
2020-07-21 13:52:23 -0500

    initial functionality file

---
+++ class_definitions/csv_to_db_emp_manage.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 7/21/2020	
+@author: Anthony Hamlin
+Program: csv_to_db_emp_manage.py
+
+This program import a csv file and created a SQLite3 database
+and then is managed through a tkinter GUI.
+"""
+
+
+def stub_main():
+    """
+    Use reST style.
+    :param x: this is what the first parameter represents
+    :returns: this is what is returned
+    :raises keyError: raises an exception
+    """
+    pass
+
+
+if __name__ == '__main__':
+    stub_main()
