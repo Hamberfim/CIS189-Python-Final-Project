75bd497aed4534b6c6602530b85d5cd68d03c2ab
Hamberfim <adhamlingithub@gmail.com>
2020-07-23 13:37:29 -0500

    trying to set the initial window size

--- class_definitions/mock_gui.py
+++ class_definitions/mock_gui.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import ttk
 
 win = tk.Tk()
+win.resizable(350, 350)
 win.attributes("-topmost", True)
 win.title("Employee Data Management")
 
@@ -20,8 +21,8 @@ def click_action():
 
 # input field
 name = tk.StringVar()
-name_entry = ttk.Entry(win, width=25, textvariable=name)
-name_entry.grid(row=1, columnspan=2)
+name_entry = ttk.Entry(win, width=30, textvariable=name)
+name_entry.grid(row=1, columnspan=3)
 name_entry.focus()
 
 # combo box selection label
@@ -37,6 +38,33 @@ chosen.current(0)
 
 # button to call click_action to change label with textvariable/StringVar
 action_btn = ttk.Button(win, text='Submit', command=click_action)
-action_btn.grid(row=3, columnspan=2)
+action_btn.grid(row=3, columnspan=3)
+
+# radio buttons global values
+COLOR1 = '#44bcd8'
+COLOR2 = '#195e83'
+COLOR3 = '#1979a9'
+
+
+# radio btn callback
+def radCall():
+    rad_select = radVar.get()
+    if rad_select == 1:
+        win.configure(background=COLOR1)
+    elif rad_select == 2:
+        win.configure(background=COLOR2)
+    elif rad_select == 3:
+        win.configure(background=COLOR3)
+
+
+radVar = tk.IntVar()
+rad1 = tk.Radiobutton(win, text='Blue ' + COLOR1, variable=radVar, value=1, command=radCall)
+rad1.grid(row=4, column=0, sticky=tk.W, columnspan=3)
+
+rad2 = tk.Radiobutton(win, text='Blue ' + COLOR2, variable=radVar, value=2, command=radCall)
+rad2.grid(row=4, column=1, columnspan=3)
+
+rad3 = tk.Radiobutton(win, text='Blue ' + COLOR3, variable=radVar, value=3, command=radCall)
+rad3.grid(row=4, column=2, sticky=tk.E, columnspan=3)
 
 win.mainloop()
