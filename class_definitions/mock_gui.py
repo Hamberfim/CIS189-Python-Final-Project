f76affb40692dc6a229534a2c85280c2d10e825f
Hamberfim <adhamlingithub@gmail.com>
2020-07-23 14:17:13 -0500

    loop radio btns

--- class_definitions/mock_gui.py
+++ class_definitions/mock_gui.py
@@ -7,7 +7,7 @@ win = tk.Tk()
 # set the initial window size (width, height)
 win.minsize(200, 150)
 win.attributes("-topmost", True)
-win.title("Employee Data Management")
+win.title("EDMA")
 
 # label to change on click
 f_label = ttk.Label(win, text="first (Top) Label")
@@ -45,24 +45,28 @@ action_btn = ttk.Button(win, text='Submit', command=click_action)
 action_btn.grid(row=3, column=0)
 
 # radio buttons global values
-COLOR1 = '#44bcd8'
-COLOR2 = '#195e83'
-COLOR3 = '#1979a9'
+# COLOR1 = '#44bcd8'
+# COLOR2 = '#195e83'
+# COLOR3 = '#1979a9'
+colors = ['#44bcd8', '#195e83', '#1979a9']
 
 
 # radio btn callback
 def radCall():
     rad_select = radVar.get()
-    if rad_select == 1:
-        win.configure(background=COLOR1)
+    if rad_select == 0:
+        win.configure(background=colors[0])
+    elif rad_select == 1:
+        win.configure(background=colors[1])
     elif rad_select == 2:
-        win.configure(background=COLOR2)
-    elif rad_select == 3:
-        win.configure(background=COLOR3)
+        win.configure(background=colors[2])
 
 
 # 3 radio btn's using one var
 radVar = tk.IntVar()
+# set to non-existing index value
+radVar.set(99)
+"""
 rad1 = tk.Radiobutton(win, text='Blue ' + COLOR1, variable=radVar, value=1,
                       command=radCall)
 rad1.grid(row=4, column=0, sticky=tk.W)
@@ -73,9 +77,17 @@ rad2.grid(row=5, column=0, sticky=tk.W)
 
 rad3 = tk.Radiobutton(win, text='Blue ' + COLOR3, variable=radVar, value=3,
                       command=radCall)
-rad3.grid(row=6, column=0, sticky=tk.W)
+rad3.grid(row=6, column=0, sticky=tk.W)"""
+# re-create the radio btn's with one loop
+for color in range(3):
+    curRad = 'rad' + str(color )
+    curRad = tk.Radiobutton(win, text='Blue ' + colors[color], variable=radVar,
+                            value=color,
+                            command=radCall)
+    # place the radio btn in the next row by adding the col to the row number
+    curRad.grid(row=4+color, sticky=tk.W, column=0)
 
-# scroll text control
+# scroll text control wrap by word not char
 scrollW = 25
 scrollH = 5
 scroll = scrolledtext.ScrolledText(win, width=scrollW, height=scrollH,
