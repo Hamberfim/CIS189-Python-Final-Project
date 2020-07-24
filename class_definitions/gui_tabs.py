104cef117e50feb3f21fe2d76b9f5eeeba19c98f
Hamberfim <adhamlingithub@gmail.com>
2020-07-24 11:43:24 -0500

    to segment out CRUD functions

--- class_definitions/gui_tabs.py
+++ class_definitions/gui_tabs.py
@@ -15,8 +15,18 @@ win.attributes("-topmost", True)
 win.title("EDMA")
 
 tab_control = ttk.Notebook(win)  # control for tab
-tab1 = ttk.Frame(tab_control)  # create tab
-tab_control.add(tab1, text="CRUD")
-tab_control.pack(expand=1, fill="both")  # pack to make visable
+create_tab = ttk.Frame(tab_control)  # create tab
+tab_control.add(create_tab, text="Create Record")  # add the tab
+
+read_tab = ttk.Frame(tab_control)  # create tab
+tab_control.add(read_tab, text="Read Record(s)")  # add the tab
+
+update_tab = ttk.Frame(tab_control)  # create tab
+tab_control.add(update_tab, text="Update Record(s)")  # add the tab
+
+delete_tab = ttk.Frame(tab_control)  # create tab
+tab_control.add(delete_tab, text="Delete Record(s)")  # add the tab
+
+tab_control.pack(expand=1, fill="both")  # pack to make visible
 
 win.mainloop()
